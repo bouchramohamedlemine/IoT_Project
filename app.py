18598c05a5834975950c97127aff0b3f5718fb9d
Bouchra Mohamed Lemine <bouchramohamedcheikh@gmail.com>
2024-12-12 00:51:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -247,5 +247,5 @@ if __name__ == '__main__':
     time.sleep(5) # wait for 5 seconds to receive the latest indoor data
 
     start_background_task()  # Start the background task
-    app.run(debug=True, port=7070)
+    app.run(debug=True)
     
